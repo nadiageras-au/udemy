bf1a2d5fb1440f6853a16563fb675543d669cec4
Nadia <nadiageras.au@gmail.com>
2021-06-12 22:24:31 +1000

    js mod

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 "use strict";
 
-const isChecked = false,
-      isClosed = false;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',''),
+    
+      personalMovieDB = {
+        count : numberOfFilms,
+        movies : {},
+        actors : {},
+        genres : [],
+        privat : false
+      };
 
-      console.log(isChecked || !isClosed);
+const a = prompt('Один из последних просмотренных фильмов?',''), 
+      b = prompt('Насколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('Насколько оцените его?','');
+      
+personalMovieDB.movies[a] = b;     
+personalMovieDB.movies[c] = d;     
+
+
+console.log(personalMovieDB);
+
